d513bc680f6ee049ec95ff389ee1d6795b4cdc1b
Dan <e.danik90@gmail.com>
2020-02-28 11:44:52 -0600

    Image clicking added

--- frontend/src/views/Result.js
+++ frontend/src/views/Result.js
@@ -2,19 +2,24 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Result = (props) => {
-	const [img, setImg] = useState();
+	// const [img, setImg] = useState();
+	const [src, setSrc] = useState(0);
 
-	// useEffect(() => {
-	// 	axios.get('http://localhost:8000/api/puzzle_pieces/read_img/')
-	// 		.then(res => console.log(res))
-	// 		.catch(err => console.log(err));
-	// }, [img])
+	const onClickHandler = (e) => {
+		e.preventDefault();
+		setSrc(props.data.imgData);
+	}
+
+	useEffect(() => {
+		setSrc(props.data.threshData);
+	}, [props.data])
+
 	return(
 		<div>
 			<h3>You have this many puzzle pieces:</h3>
 			<h1>{props.data.count}</h1>
-			<img id='Outlines' style={{width: 500 + 'px'}} src={"data:image/png;base64," + props.data.imgData}></img>
-			<img id='Threshold' style={{width: 500 + 'px'}} src={"data:image/png;base64," + props.data.threshData}></img>
+			<img id='Outlines' style={{width: 500 + 'px'}} src={"data:image/png;base64," + src} onClick = {onClickHandler}></img>
+			{/* <img id='Threshold' style={{width: 500 + 'px'}} src={"data:image/png;base64," + props.data.threshData}></img> */}
 		</div>
 	)
 }
